3f3d0feca2ff34ecbfc906260f895a6bb6b1bb65
Sean Denny <nektromc@gmail.com>
2018-04-07 03:58:03 -0400

    add util js

--- js/app.js
+++ js/app.js
@@ -6,3 +6,14 @@
  */
 //
 'use strict';
+//
+import * as a from "./util.js";
+
+//
+const modsList = ['kanto'];
+
+//
+a.log(a.LogType.INFO, 'Test Info log.');
+a.log(a.LogType.GOOD, 'Test Good log.');
+a.log(a.LogType.WARN, 'Test Warn log.');
+a.log(a.LogType.ERROR, 'Test Error log.');
---
+++ js/util.js
@@ -0,0 +1,28 @@
+/**
+ */
+//
+'use strict';
+
+//
+const LOG_ELEMENT = document.getElementById('log');
+
+export const can = document.getElementById('screen');
+export const con = can.getContext('2d');
+export const pen = new Pencil(con);
+
+//
+export const LogType = {
+    INFO: ['INFO', '000000'],
+    GOOD: ['GOOD', '4CAF50'],
+    WARN: ['WARN', 'FF9800'],
+    ERROR: ['ERROR', 'F44336']
+};
+
+//
+export function log(t, m) {
+    LOG_ELEMENT.innerHTML += `<div style="color:#${t[1]}">[${t[0]}]: ${m}</div>`;
+}
+export function assign(o, k, v) {
+    o[k] = v;
+    return o;
+}
