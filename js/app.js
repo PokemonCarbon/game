b00093e484c812f20223a991c41202cd6e708dfb
Sean Denny <nektromc@gmail.com>
2018-04-13 13:43:39 -0400

    app : announce where the game is loading

--- js/app.js
+++ js/app.js
@@ -24,5 +24,7 @@ Promise.all(modsList.map((v,i) => Region.build(v)))
 .then(x => {
     a.log(a.LogType.GOOD, `Successfully loaded Regions: [${x.map(v => `'${v.name}'`)}]`);
     a.log(a.LogType.GOOD, `Starting game!`);
+    a.log(a.LogType.INFO, `Starting in first Region: ${x[0].name}`);
+    a.log(a.LogType.INFO, `Starting in first Map: ${x[0].maps[0]}`)
     console.log(x);
 });
