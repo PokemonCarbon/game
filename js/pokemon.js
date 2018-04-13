b4759fb06549eb0dc622492a8eebcb2e030d49b0
Sean Denny <nektromc@gmail.com>
2018-04-13 13:40:43 -0400

    pokemon : use util.parse_ini to build pokemon.ini

--- js/pokemon.js
+++ js/pokemon.js
@@ -15,10 +15,6 @@ export class Pokemon {
 }
 export const build = async (reg, id) => {
     a.log(a.LogType.INFO, `Creating Pokemon: ${reg}:${id}`);
-    const p = new Pokemon();
-    const _1 = await fetch(`../${reg}/pokemon/${id}/pokemon.ini`).then(x => x.text());
-    const _2 = _1.split('\n').map(v => v.split('='));
-    const _3 = _2.reduce((ac,cv) => (cv.length === 1) ? (ac) : (a.assign(ac, ...cv)), {});
-    Object.assign(p, _3);
-    return p;
+    const pki = await a.ffetch(`../${reg}/pokemon/${id}/pokemon.ini`, 'text');
+    return new Pokemon(a.pipe(pki, a.parse_ini));
 }
