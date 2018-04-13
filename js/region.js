cd7bd7871632774e1c6f062e86e465fff254e9ef
Sean Denny <nektromc@gmail.com>
2018-04-13 06:32:33 -0400

    add manifest parsing for adding type definitions

--- js/region.js
+++ js/region.js
@@ -18,8 +18,21 @@ export const build = async (id) => {
     a.log(a.LogType.INFO, `Creating Region: ${id}`);
     const manifest = await fetch(`../${id}/manifest.json`).then(x => x.json());
     const { api_version, name, version } = manifest;
+    const types = new Map();
     const pokemon = new Map();
 
+    if (manifest.types !== undefined) {
+        for (const t in manifest.types) {
+            const [ name, weakness, resistance, immunity, special ] = manifest.types[t];
+            types.set(t, new Type.Type({
+                name,
+                weakness: weakness || [],
+                resistance: resistance || [],
+                immunity: immunity || [],
+                special: special || false
+            }));
+        }
+    }
     if (manifest.pokemon !== undefined) {
         const pk_ar = await Promise.all(manifest.pokemon.map(v => Pokemon.build(id, v)));
         for (const p of pk_ar) {
@@ -27,6 +40,6 @@ export const build = async (id) => {
         }
     }
 
-    const r = new Region({ api_version, name, version, pokemon });
+    const r = new Region({ api_version, name, version, types, pokemon });
     return r;
 }
---
+++ js/type.js
@@ -0,0 +1,15 @@
+/**
+ */
+//
+'use strict';
+//
+import * as a from "./util.js";
+
+//
+export class Type {
+    constructor(args) {
+        for (const o in args) {
+            this[o] = args[o];
+        }
+    }
+}
