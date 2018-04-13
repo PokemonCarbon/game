e1f5c4246db5d86e9de48fca1e28158e05282cfe
Sean Denny <nektromc@gmail.com>
2018-04-13 13:48:54 -0400

    app : load first map from entity registry

--- js/app.js
+++ js/app.js
@@ -21,11 +21,12 @@ a.log(a.LogType.ERROR, 'Test Error log.');
 
 //
 Promise.all(modsList.map((v,i) => Region.build(v)))
-.then(x => {
+.then(async x => {
     a.log(a.LogType.GOOD, `Successfully loaded Regions: [${x.map(v => `'${v.name}'`)}]`);
     a.log(a.LogType.GOOD, `Starting game!`);
     a.log(a.LogType.INFO, `Starting in first Region: ${x[0].name}`);
     a.log(a.LogType.INFO, `Starting in first Map: ${x[0].maps[0]}`)
     console.log(x);
     console.log(a.regions);
+    console.log(await a.pipe(x[0], Region.get('maps')(x[0].maps[0])));
 });
