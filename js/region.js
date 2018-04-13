1ea52b72aa4f9d22f8d8a2a4218ffc96ea9a3d49
Sean Denny <nektromc@gmail.com>
2018-04-12 21:00:41 -0400

    region : parse all manifest attributes instead of O.a properties

--- js/region.js
+++ js/region.js
@@ -8,16 +8,25 @@ import * as Pokemon from "./pokemon.js";
 
 //
 export class Region {
-    constructor() {
-        //
+    constructor(args) {
+        for (const o in args) {
+            this[o] = args[o];
+        }
     }
 }
 export const build = async (id) => {
     a.log(a.LogType.INFO, `Creating Region: ${id}`);
-    const r = new Region();
-    Object.assign(r, await fetch(`../${id}/manifest.json`).then(x => x.json()));
-    if (r.pokemon !== undefined) {
-        r.pokemon_data = await Promise.all(r.pokemon.map(v => Pokemon.build(id, v)));
+    const manifest = await fetch(`../${id}/manifest.json`).then(x => x.json());
+    const { api_version, name, version } = manifest;
+    const pokemon = new Map();
+
+    if (manifest.pokemon !== undefined) {
+        const pk_ar = await Promise.all(manifest.pokemon.map(v => Pokemon.build(id, v)));
+        for (const p of pk_ar) {
+            pokemon.set(p.InternalName.toLowerCase(), p);
+        }
     }
+
+    const r = new Region({ api_version, name, version, pokemon });
     return r;
 }
