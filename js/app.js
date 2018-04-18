7121f2e3156c37db973f8baab4c60cd1d9433add
Sean Denny <nektromc@gmail.com>
2018-04-18 01:29:35 -0400

    app : static import of util props

--- js/app.js
+++ js/app.js
@@ -3,21 +3,21 @@
 //
 'use strict';
 //
-import * as a from "./util.js";
+import { pen, pipe, log, LogType, game, player } from "./util.js";
 import * as Region from "./region.js";
 
 //
 const modsList = ['kanto'];
 
 //
-a.log(a.LogType.INFO, 'Test Info log.');
-a.log(a.LogType.GOOD, 'Test Good log.');
-a.log(a.LogType.WARN, 'Test Warn log.');
-a.log(a.LogType.ERROR, 'Test Error log.');
+log(LogType.INFO, 'Test Info log.');
+log(LogType.GOOD, 'Test Good log.');
+log(LogType.WARN, 'Test Warn log.');
+log(LogType.ERROR, 'Test Error log.');
 
 //
 Promise.all(modsList.map((v,i) => Region.build(v)))
 .then(async x => {
-    a.log(a.LogType.GOOD, `Successfully loaded Regions: [${x.map(v => `'${v.name}'`)}]`);
-    a.log(a.LogType.GOOD, `Starting game!`);
+    log(LogType.GOOD, `Successfully loaded Regions: [${x.map(v => `'${v.name}'`)}]`);
+    log(LogType.GOOD, `Starting game!`);
 });
