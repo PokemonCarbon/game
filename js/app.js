6ba5d4f8bdde65d8d86a5e82959abd7ac4b9be25
Sean Denny <nektromc@gmail.com>
2018-06-01 16:29:26 -0400

    app : create game and start drawing

--- js/app.js
+++ js/app.js
@@ -5,6 +5,8 @@
 //
 import { pen, pipe, log, LogType, pipe_async } from "./util.js";
 import * as Region from "./region.js";
+import * as Game from "./game.js";
+import * as Player from "./player.js";
 
 //
 const modsList = ["kanto"];
@@ -20,4 +22,11 @@ Promise.all(modsList.map((v) => Region.build(v)))
 .then(async (x) => {
     log(LogType.GOOD, `Successfully loaded Regions: [${x.map(v => `"${v.name}"`)}]`);
     log(LogType.GOOD, "Starting game!");
+
+    const game = pipe(pipe({}, Player.build), Game.build);
+
+    requestAnimationFrame(function render() {
+        pipe(game, Game.draw);
+        requestAnimationFrame(render);
+    });
 });
