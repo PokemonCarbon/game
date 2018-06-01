cb89147cdf060abc4602ce57e54428b31ffccf80
Sean Denny <nektromc@gmail.com>
2018-06-01 16:27:11 -0400

    player : add player module

---
+++ js/player.js
@@ -0,0 +1,35 @@
+/**
+ */
+//
+"use strict";
+//
+import { pipe_async, geometry } from "./util.js";
+import * as Region from "./region.js";
+
+//
+export class Player {
+    constructor(args) {
+        for (const o in args) {
+            this[o] = args[o];
+        }
+    }
+}
+export const build = () => {
+    return new Player({
+        name: "Meghan",
+        gender: null,
+        imgs: [null,null,null],
+        map: null,
+        pos: new geometry.Point(0,0),
+        direction: null
+    });
+};
+export const setMap = (reg, mapID) => async (player) => {
+    player.map = await pipe_async(reg, Region.get("maps")(mapID));
+    return player;
+};
+export const setPos = (x, y) => (player) => {
+    player.pos.x = x;
+    player.pos.y = y;
+    return player;
+};
