2e450f408bee3f3ff6094dfe9f8abd64b451155c
Sean Denny <nektromc@gmail.com>
2018-06-01 16:24:49 -0400

    import : update modules.js version

--- js/imports.js
+++ js/imports.js
@@ -8,7 +8,7 @@ import _pipeline from "https://unpkg.com/pipeline-operator/index.js";
 export const pipeline = _pipeline;
 
 //
-import * as _geometry from "https://cdn.rawgit.com/Nektro/modules.js/453faa5/src/geometry.js";
+import * as _geometry from "https://cdn.rawgit.com/Nektro/modules.js/b7e21ce/src/geometry.js";
 export const geometry = _geometry;
 
-export { Pencil } from "https://cdn.rawgit.com/Nektro/modules.js/453faa5/src/pencil.js";
+export { Pencil } from "https://cdn.rawgit.com/Nektro/modules.js/b7e21ce/src/pencil.js";
