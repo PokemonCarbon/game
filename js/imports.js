80158969ebe3b0b3b59bbe1245944d9863ffd405
Sean Denny <nektromc@gmail.com>
2018-06-02 06:22:43 -0400

    imports : add Loop module

--- js/imports.js
+++ js/imports.js
@@ -11,3 +11,4 @@ export const geometry = _geometry;
 export { pipe, pipe_async }                    from "https://cdn.rawgit.com/Nektro/modules.js/85d42e3/src/pipe.js";
 export { Pencil }                              from "https://cdn.rawgit.com/Nektro/modules.js/85d42e3/src/pencil.js";
 export { isKeyDown, addKeyDownListener, Keys } from "https://cdn.rawgit.com/Nektro/modules.js/85d42e3/src/keyboard.js";
+export { Loop }                                from "https://cdn.rawgit.com/Nektro/modules.js/85d42e3/src/loop.js";
