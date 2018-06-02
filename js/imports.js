dfc6784ed67efddece7556f02a8cf4e2753171de
Sean Denny <nektromc@gmail.com>
2018-06-02 01:01:05 -0400

    imports : add keyboard module and update version

--- js/imports.js
+++ js/imports.js
@@ -4,10 +4,12 @@
 "use strict";
 
 //
-export { pipe, pipe_async }                    from "https://cdn.rawgit.com/Nektro/modules.js/16ef845/src/pipe.js";
+export { pipe, pipe_async }                    from "https://cdn.rawgit.com/Nektro/modules.js/85d42e3/src/pipe.js";
 
 //
-import * as _geometry                          from "https://cdn.rawgit.com/Nektro/modules.js/16ef845/src/geometry.js";
+import * as _geometry                          from "https://cdn.rawgit.com/Nektro/modules.js/85d42e3/src/geometry.js";
 export const geometry = _geometry;
 
-export { Pencil }                              from "https://cdn.rawgit.com/Nektro/modules.js/16ef845/src/pencil.js";
+export { Pencil }                              from "https://cdn.rawgit.com/Nektro/modules.js/85d42e3/src/pencil.js";
+
+export { isKeyDown, addKeyDownListener, Keys } from "https://cdn.rawgit.com/Nektro/modules.js/85d42e3/src/keyboard.js";
