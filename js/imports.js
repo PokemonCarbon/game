217ee892fe237d56ed013555f8faea34bbb1b245
Sean Denny <nektromc@gmail.com>
2018-06-02 06:22:27 -0400

    imports : move pipe import down and remove extra newline

--- js/imports.js
+++ js/imports.js
@@ -4,12 +4,10 @@
 "use strict";
 
 //
-export { pipe, pipe_async }                    from "https://cdn.rawgit.com/Nektro/modules.js/85d42e3/src/pipe.js";
-
-//
 import * as _geometry                          from "https://cdn.rawgit.com/Nektro/modules.js/85d42e3/src/geometry.js";
 export const geometry = _geometry;
 
+//
+export { pipe, pipe_async }                    from "https://cdn.rawgit.com/Nektro/modules.js/85d42e3/src/pipe.js";
 export { Pencil }                              from "https://cdn.rawgit.com/Nektro/modules.js/85d42e3/src/pencil.js";
-
 export { isKeyDown, addKeyDownListener, Keys } from "https://cdn.rawgit.com/Nektro/modules.js/85d42e3/src/keyboard.js";
