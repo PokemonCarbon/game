1e1ef70bb0ea1ca252feaf8160d36563dc87f083
Sean Denny <nektromc@gmail.com>
2018-06-03 10:54:33 -0400

    imports : update modules.js with geometry update

--- js/imports.js
+++ js/imports.js
@@ -4,11 +4,11 @@
 "use strict";
 
 //
-import * as _geometry                          from "https://cdn.rawgit.com/Nektro/modules.js/85d42e3/src/geometry.js";
+import * as _geometry                          from "https://cdn.rawgit.com/Nektro/modules.js/04673d0/src/geometry.js";
 export const geometry = _geometry;
 
 //
-export { pipe, pipe_async }                    from "https://cdn.rawgit.com/Nektro/modules.js/85d42e3/src/pipe.js";
-export { Pencil }                              from "https://cdn.rawgit.com/Nektro/modules.js/85d42e3/src/pencil.js";
-export { isKeyDown, addKeyDownListener, Keys } from "https://cdn.rawgit.com/Nektro/modules.js/85d42e3/src/keyboard.js";
-export { Loop }                                from "https://cdn.rawgit.com/Nektro/modules.js/85d42e3/src/loop.js";
+export { pipe, pipe_async }                    from "https://cdn.rawgit.com/Nektro/modules.js/04673d0/src/pipe.js";
+export { Pencil }                              from "https://cdn.rawgit.com/Nektro/modules.js/04673d0/src/pencil.js";
+export { isKeyDown, addKeyDownListener, Keys } from "https://cdn.rawgit.com/Nektro/modules.js/04673d0/src/keyboard.js";
+export { Loop }                                from "https://cdn.rawgit.com/Nektro/modules.js/04673d0/src/loop.js";
