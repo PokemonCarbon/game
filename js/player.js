91eff85ac592c6521c016bc711b2a418f24e20ec
Sean Denny <nektromc@gmail.com>
2018-06-03 11:10:11 -0400

    player : add Player.update

--- js/player.js
+++ js/player.js
@@ -3,9 +3,15 @@
 //
 "use strict";
 //
-import { geometry, pipe_async } from "./imports.js";
-import { Direction } from "./util.js";
+import { geometry, pipe_async, isKeyDown, Keys, pipe } from "./imports.js";
+import { Direction, WIDTH, HEIGHT, pen } from "./util.js";
 import * as Region from "./region.js";
+import * as Mapp from "./map.js";
+
+//
+geometry.Point.prototype.sum = function sum() {
+    return this.x + this.y;
+};
 
 //
 export class Player {
@@ -34,4 +40,21 @@ export const setPos = (x, y) => (player) => {
     player.pos.x = x;
     player.pos.y = y;
     return player;
+};
+export const update = (player) => {
+    if (isKeyDown(Keys.ArrowRight)) if (pipe(player.map, Mapp.isZoneWalkable(player.pos.x + 1, player.pos.y))) if (player.direction === Direction.Right) if (player.vel.sum() === 0) player.vel.x =  20;
+    if (isKeyDown(Keys.ArrowDown))  if (pipe(player.map, Mapp.isZoneWalkable(player.pos.x, player.pos.y + 1))) if (player.direction === Direction.Down)  if (player.vel.sum() === 0) player.vel.y =  20;
+    if (isKeyDown(Keys.ArrowLeft))  if (pipe(player.map, Mapp.isZoneWalkable(player.pos.x - 1, player.pos.y))) if (player.direction === Direction.Left)  if (player.vel.sum() === 0) player.vel.x = -20;
+    if (isKeyDown(Keys.ArrowUp))    if (pipe(player.map, Mapp.isZoneWalkable(player.pos.x, player.pos.y - 1))) if (player.direction === Direction.Up)    if (player.vel.sum() === 0) player.vel.y = -20;
+    //
+    if (player.vel.x !== 0) {
+        if (player.vel.x % 2 === 0)       player.pos.x += .1 * Math.sign(player.vel.x);
+        if (Math.abs(player.vel.x) === 1) player.pos.x = Math.round(player.pos.x);
+    }
+    if (player.vel.y !== 0) {
+        if (player.vel.y % 2 === 0)       player.pos.y += .1 * Math.sign(player.vel.y);
+        if (Math.abs(player.vel.y) === 1) player.pos.y = Math.round(player.pos.y);
+    }
+    //
+    player.vel.sub(new geometry.Point(...player.vel.spread().map(v => Math.sign(v))));
 };
